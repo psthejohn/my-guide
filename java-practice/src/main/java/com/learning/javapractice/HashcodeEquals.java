36f2566c715781ac76d990384f321f4a669525fa
Ashutosh Singh <ashutosh.9.singh@stackroute.in>
2018-09-17 14:07:44 +0530

    java practice module added

---
+++ java-practice/src/main/java/com/learning/javapractice/HashcodeEquals.java
@@ -0,0 +1,8 @@
+package com.learning.javapractice;
+
+public class HashcodeEquals {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is to demonstrate hashcode and equals");
+	}
+}
