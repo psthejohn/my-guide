4325138deb51a76cb4fab702f6d6f5a9b0053c72
psthejohn <36690315+psthejohn@users.noreply.github.com>
2019-08-26 23:07:47 +0530

    Update index.js
    adding some array operation methods.

--- learning-javascript/002-js-basics/005-arrays/index.js
+++ learning-javascript/002-js-basics/005-arrays/index.js
@@ -2,3 +2,22 @@ let selectedColors = ['red', 'blue'];
 console.log(selectedColors);
 console.log(selectedColors[0]);
 console.log(selectedColors.length);
+
+// Pushing an element to array
+selectedColors.push('black');
+console.log(selectedColors[2]); // it adds black color at last
+
+// Removing the last element from array
+selectedColors.pop();
+console.log(selectedColors);// it should remove black color
+
+// Removing from front of an array
+selectedColors.shift();
+console.log(selectedColors); // it removes red color from front
+
+// Adding an element to front of array
+selectedColors.unshift('red');
+console.log(selectedColors); // it adds red color in front
+
+// Finding index of an element in array
+console.log(selectedColors.indexOf('red'));// it checks position of red in array
